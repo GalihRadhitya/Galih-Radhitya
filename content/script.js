78c142f08724014b2a59aa387e63d7320cdafd9f
GalihRadhitya <kepoangker@gmail.com>
2026-01-10 22:56:09 +0700

    update

--- content/script.js
+++ content/script.js
@@ -21,10 +21,44 @@ window.addEventListener("scroll", () => {
     });
 
     navLinks.forEach(link => {
-        link.classList.remove("active"):
+        link.classList.remove("active");
         if(link.getAttribute("href") === `#${current}`) {
             link.classList.add("active");
         }
     })
 });
 
+document.querySelector(".btn.primary").addEventListener("click", () =>{
+    window.open("CV_Galih.pdf", "_blank");
+});
+
+document.querySelector(".btn.secondary").addEventListener("click", () =>{
+    window.location.href = "mailto:kepoangker@gmail.com";
+});
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("show");
+        }
+    });
+}, {threshold: 0.2});
+
+document.querySelectorAll(".skills, .interested, .hero").forEach(el =>{
+    observer.observe(el);
+});
+
+const text = "Galih Radhitya Wira";
+let i = 0;
+const target = document.querySelector(".text h2 span");
+
+function typing() {
+    if (i < text.length){
+        target.textContent += text.charAt(i);
+        i++;
+        setTimeout(typing, 100);
+    }
+}
+
+target.textContent = "";
+typing();
